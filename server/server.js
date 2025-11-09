775ab7aa767748f01e07f580ef0d5321dd9e501d
Hassan Osama <73666211+hassanosama5@users.noreply.github.com>
2025-11-09 17:43:48 +0200

    Initial commit bsmellah

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 5000;
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
